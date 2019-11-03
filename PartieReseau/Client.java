48b96b9f6b676ba2009e982bf6ea15bcc5f4489f
momo007Dev <m.valentin@students.ephec.be>
2019-11-03 23:22:44 +0100

    Version Fonctionnel (avec couleur) Linux uniquement

--- PartieReseau/Client.java
+++ PartieReseau/Client.java
@@ -1,45 +1,59 @@
 package chat;
 
 import java.io.*; 
-import java.net.*; 
+import java.net.*;
+import java.util.InputMismatchException;
 import java.util.Scanner; 
   
 // Client class 
 public class Client  
 { 
+	//Colours on ouput => ONLY WORKS ON LINUX (maybe mac ?) NOT WINDOWS
+	protected static final String red    = "\u001B[31m";
+	protected static final String blue   = "\u001B[34m";
+	protected static final String reset  = "\u001B[0m";
+	protected static final String purple = "\u001B[35m";
+	
     public static void main(String[] args) throws IOException  
     { 
         try
         { 
             Scanner scn = new Scanner(System.in); 
               
-            // Retrieving IP address and port from user input:
-            
-            System.out.println("IP address ?");
-    		String ip = scn.nextLine();
-    		System.out.println("Port number ?");
+            // Retrieving port and IP address from user input:
+            System.out.println(purple+"Port number ?"+reset);
     		int port = scn.nextInt();
+    		System.out.println("Client> "+purple+port);
+            System.out.println(red+"IP address ?"+reset);
+    		String ip = scn.nextLine();
+    		if (ip.length()==0) {
+    			ip = scn.nextLine();
+    		}
+    		System.out.println("Client> "+red+ip+reset);
+    		
       
             // establish the connection with the server with IP and Port from user input
-            Socket sock = new Socket(ip, port); 
+            System.out.println(blue+"Waiting to connect to server..."+reset);
+    		Socket sock = new Socket(ip, port);
+    		System.out.println(red+"Connected !"+reset);
+    		System.out.println("__________________________");
       
             // In and out streams => Information received (inputStream) and sent (outputStream) :
             DataInputStream in = new DataInputStream(sock.getInputStream()); 
             DataOutputStream out = new DataOutputStream(sock.getOutputStream()); 
       
-            //Little test to show (on server side) the number of connections 
-            System.out.println(in.readUTF()); 
-            String test = "con"; 
-            out.writeUTF(test);
+            
             //Infinite loop => "Until user input (you) types "end", read whatever
             while (true)  
             { 
+
                 System.out.println(in.readUTF());//Reads the server's output (stream) 
-                String tosend = scn.nextLine();//Holds user input in a variale "tosend" 
-                out.writeUTF(tosend);//Sends to server characters contained in String "tosend" 
-                  
+                String sendToServer = scn.nextLine();//Holds user input in a variale "tosend"
+                out.writeUTF(sendToServer);//Sends to server characters contained in String "tosend"
+                System.out.println(blue+"Client> "+red+sendToServer+reset);
+                 
                 // If the client (you) sends "end" => Close the connection
-                if(tosend.equals("end")) 
+                if(sendToServer.equals("end")) 
                 { 
                     System.out.println("Closing this connection : " + sock); 
                     sock.close(); 
@@ -48,8 +62,8 @@ public class Client
                 } 
                   
                 // Displays the answer from the server (Date-Time-Number of connected clients,...etc)
-                String received = in.readUTF(); 
-                System.out.println(received); 
+                String receivedFromServer = in.readUTF(); 
+                System.out.println(receivedFromServer); 
             } 
               
             // closing resources 
@@ -57,8 +71,14 @@ public class Client
             in.close(); 
             out.close();
             System.exit(0);
-        }catch(Exception e){ 
-            e.printStackTrace(); 
-        } 
+            
+        }
+        catch(IOException e){ 
+            //e.printStackTrace(); => To show errors (I don't want users to see thoses...)
+            System.out.println(red+"Can't reach server...Check Ip/port and connectivity"+reset);
+        }
+        catch(InputMismatchException a) {
+        	System.out.println(red+"FATAL ERROR :"+reset+purple+" IP and Port must be Integer"+reset);
+        }
     } 
 } 
--- PartieReseau/Server.java
+++ PartieReseau/Server.java
@@ -5,13 +5,10 @@ import java.net.*;
 import java.text.*;
 import java.util.*;
 
-/* A implementer :
- * -) Stocker le nombre de connections (joueurs)--OK
- * -) Limiter ce nombre a 2 max. --OK
- * -) Exit le program si le nombre est egale a 0 (0 clients) ou plus que 2 (Plus de 2 clients/joueurs) --OK
- * -) Documenter le code ... --Pas fini
- * Envoyer des infos entre clients --Reste a implementer
- */
+/*
+ * @Author Morgan Valentin
+ * @Source code used to understand => https://www.geeksforgeeks.org/introducing-threads-socket-programming-java/
+ * */
 
 public class Server  
 { 	
@@ -24,61 +21,78 @@ public class Server
 	*/
 	protected static int num=0;//Used for user to add 5 (methode add5) to this num variable. (Testing purposes)
 	
-    public static void main(String[] args) throws IOException  { 
+	//Colours on ouput => ONLY WORKS ON LINUX (maybe mac ?) NOT WINDOWS
+	protected static final String red    = "\u001B[31m";
+	protected static final String blue   = "\u001B[34m";
+	protected static final String reset  = "\u001B[0m";
+	protected static final String purple = "\u001B[35m";
+	protected static final String yellow = "\u001B[33m";
+	protected static final String white  = "\u001B[37m";
+	
+    public static void main(String[] args) throws IOException, InputMismatchException  { 
     	//Selecting port number from user to use => Plus tard par interface graphique
     	//Attention => Le port du client devra etre identique sinon il ne "verra" pas le server !!
-    	
-    	Scanner scn = new Scanner(System.in);//Get port number from user input
-    	System.out.println("Port number ?");
-		int port = scn.nextInt();
-        ServerSocket servSock = new ServerSocket(port);//Creating a new serverSocket with port given by user. 
-        
-        System.out.println("Server started !");
-        System.out.println("Waiting for player(s) to connect");
-          
-        // Infinite loop => Waiting that a client connects {accept() method}
-        
-        while (true)  { 
-            Socket sock = null; //Socket for client communication => "Active" socket
+    	try {
+    		Scanner userInput = new Scanner(System.in);//Get port number from user input
+        	System.out.println(purple+"Port number ?"+reset);
+    		int port = userInput.nextInt();
+            ServerSocket servSock = new ServerSocket(port);//Creating a new serverSocket with port given by user. 
+            
+            System.out.println(red+"Server started !"+reset);
+            System.out.println("Waiting for player(s) to connect");
               
-            try { 
-                // socket object to receive incoming client requests => "Passive" socket
-            	
-                sock = servSock.accept();
-                
+            // Infinite loop => Waiting that a client connects {accept() method}
+            
+            while (true)  { 
+                Socket sock = null; //Socket for client communication => "Active" socket
                   
-                System.out.println("A new client is connected via " + sock);//To see which ip the client used to connect 
-                  
-                // In and out streams => Information received (inputStream) and sent (outputStream) :
-                
-                DataInputStream in = new DataInputStream(sock.getInputStream()); 
-                DataOutputStream out = new DataOutputStream(sock.getOutputStream()); 
-                  
-                System.out.println("Assigning new thread for this client"); 
-                System.out.println("-----------------------------------------");	
-                // create a new thread object => Nouveau "processus" en quelque sorte
-                Thread t = new ClientHandler(sock, in, out); 
-  
-                // Demarre le processus t (gestion des clients) et mais en "pause" la suite des instructions du server.
-                t.start(); //Start method search the run() method from the Threat class heritence 
-                  
-            } 
-            catch (Exception e){ 
-            	sock.close();
-                e.printStackTrace(); 
-            } 
-        } 
+                try { 
+                    // socket object to receive incoming client requests => "Passive" socket
+                	
+                    sock = servSock.accept();
+                    
+                      
+                    System.out.println("A new client is connected via " +yellow+ sock.getLocalAddress()+reset);//To see which ip the client used to connect 
+                      
+                    // In and out streams => Information received (inputStream) and sent (outputStream) :
+                    
+                    DataInputStream in = new DataInputStream(sock.getInputStream()); 
+                    DataOutputStream out = new DataOutputStream(sock.getOutputStream()); 
+                      
+                    System.out.println("Assigning new thread for this client"); 
+                    System.out.println("-----------------------------------------");	
+                    // create a new thread object => Nouveau "processus" en quelque sorte
+                    Thread t = new ClientHandler(sock, in, out); 
+      
+                    // Demarre le processus t (gestion des clients) et mais en "pause" la suite des instructions du server.
+                    t.start(); //Start method search the run() method from the Threat class heritence 
+                      
+                } 
+                catch (IOException e){ 
+                	servSock.close();
+                	userInput.close();
+                	sock.close();
+                    e.printStackTrace();
+                }
+            }
+    	}
+    	catch(InputMismatchException a) {
+        	System.out.println(Server.red+"FATAL ERROR :"+Server.reset+Server.purple+" IP and Port must be Integer"+Server.reset);
+        }
+    	catch(EOFException e) {
+    		System.out.println("Et on ne dit même pas au revoir ? Je vois...");
+    	}
+    	 
     } 
 }
 //To "create" a new thread (process ?) either implement interface runnable OR inherite from the "Thread" class
 class ClientHandler extends Thread  
 { 
-    DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");//Used for user to ask for Date
-    DateFormat fortime = new SimpleDateFormat("hh:mm:ss");//Used for user to ask for Time
+    DateFormat myDateFormat = new SimpleDateFormat("EEEE,dd-MMMM-YYYY");//Used for user to ask for Date
+    DateFormat myTimeFormat = new SimpleDateFormat("hh:mm:ss");//Used for user to ask for Time
     final DataInputStream in; 
     final DataOutputStream out; 
     final Socket sock; 
-    //private int num=0;
     private static int numberOfClientsConnected=0;
       
   
@@ -95,29 +109,31 @@ class ClientHandler extends Thread
          * Des que le 3eme client se connecte, il recoit toute une serie d erreur (a catcher et masquer de l utilisateur)
          * Cependant, le server se couppe correctement.
          * Les 2 autres clients (client 1 et 2) ne voit pas de message disant que la connection est couper ==>>Optimiser
-         * Mais une fois une commande entrer => Erreur disant que le serveur n est plus joignable.
+         * Mais une fois une commande entrer => Erreur disant que le serveur n'est plus joignable.
          */
     } 
   
     @Override
-    public void run()  
-    { 
-        String received; 
-        String toreturn;
-        while (true)  
-        { 
+    public void run()  { 
+        String receivedFromClient; 
+        String sendToClient;
+        final String name = Thread.currentThread().getName();
+        final long id = Thread.currentThread().getId(); 
+        Date date = new Date();//Creating Date object (used in Time and Date)
+        while (true)  { 
             try { 
   
                 // Show the different possibilities (options) available
-                out.writeUTF("What do you want?[Date | Time | add5 | con(Show number of clients connected]..\n"+ 
-                            "Type end to terminate connection."); 
+                out.writeUTF("Welcome "+Server.blue+name+Server.reset+" your id is : "+Server.blue+id+Server.reset+"\n"+
+                		"What do you want? Options =>"+Server.purple+"[Date | Time | add5 | con(showNumOfClientConnected)]"+Server.reset+"\n"+ 
+                            "Type "+Server.red+"'end'"+Server.reset+" to terminate connection.\n"
+                		+"---------------------------"); 
                   
                 // receive the answer from client 
-                received = in.readUTF(); 
+                receivedFromClient = in.readUTF(); 
                   
-                if(received.equals("end")) 
-                {  
-                    System.out.println("Client " + this.sock + " sends end command..."); 
+                if(receivedFromClient.equals("end")) {  
+                    System.out.println("Client " +Server.yellow+ this.sock.getInetAddress() +Server.reset+ " sends end command..."); 
                     System.out.println("Closing THIS connection."); 
                     this.sock.close();
                     numberOfClientsConnected--;
@@ -125,63 +141,67 @@ class ClientHandler extends Thread
                     break; 
                 } 
                   
-                // creating Date object 
-                Date date = new Date(); 
                   
                 // write on output stream based on the 
                 // answer from the client 
-                switch (received) { 
+                switch (receivedFromClient) { 
                 //The "case" correspond to what the client sent (if none of the command => default : "invalid output"
                   
                     case "Date" : 
                     	//Shows the date
-                        toreturn = fordate.format(date); 
-                        out.writeUTF("Server> "+toreturn);
-                        System.out.println(toreturn);
+                    	sendToClient = myDateFormat.format(date); 
+                        out.writeUTF(Server.yellow+"Server>"+Server.white+" "+sendToClient+Server.reset+"\n"+"-------------------");
+                        System.out.println(sendToClient);//Visualisation des donnees envoyes au client cote serveur
                         break; 
                           
                     case "Time" : 
                     	//Shows the Time
-                        toreturn = fortime.format(date); 
-                        out.writeUTF("Server> "+toreturn); 
-                        System.out.println(toreturn);
+                    	sendToClient = myTimeFormat.format(date); 
+                        out.writeUTF(Server.yellow+"Server>"+Server.white+" "+sendToClient+Server.reset+"\n"+"-------------------"); 
+                        System.out.println(sendToClient);
                         break;
                         
                     case "add5" : 
                     	
                     	//Adds 5 to the "num" variable and displays its value
                         Server.num += 5;
-                        out.writeUTF("Server> Num = "+Server.num); 
+                        out.writeUTF(Server.yellow+"Server>"+Server.white+" Num = "+Server.num+Server.reset+"\n"+"-------------------"); 
                         System.out.println("Value of num : "+Server.num);
                         break; 
                         
                     case "con" : 
                     	//Show how many clients are connected to the server
-                        out.writeUTF("Server> Number of connections : "+numberOfClientsConnected); 
+                        out.writeUTF(Server.yellow+"Server>"+Server.white+" Number of connections : "+numberOfClientsConnected+Server.reset+"\n"+"-------------------"); 
                         System.out.println("Num of connec : "+numberOfClientsConnected);
                         break;
+                        
+                    case "" :
+                    	out.writeUTF("");
+                    	break;
+                    	
                           
                     default: 
-                        out.writeUTF("Invalid input"); 
+                        out.writeUTF("Invalid input");
                         break; 
                 } 
             } catch (IOException e) { 
-                e.printStackTrace(); 
-            } 
+                //e.printStackTrace();
+            	System.out.println(Server.red+"Et on ne dit même pas au revoir ?"+Server.reset+Server.blue+" Je vois..."+Server.reset);
+            	System.exit(0);
+            }
         } 
           
         testConnection();//Tests if there's 0 client OR More than 2 connected => If so Closes ressources and Exit program.
     }
     public void testConnection() {
-    	try
-        { 
+    	try { 
             // closing resources 
             if (numberOfClientsConnected==0) {
             	System.out.println("No more clients...ENDING Server!");
             	this.in.close(); 
                 this.out.close(); 
                 this.sock.close();
-            	 System.exit(0);//Close console (terminal) waiting error.
+            	System.exit(0);//Exit program => Thread stop
             }
             if (numberOfClientsConnected>2) {
             	System.out.println("More than 2 clients... ENDING Server!");
@@ -192,8 +212,8 @@ class ClientHandler extends Thread
             }
               
         }catch(IOException e){ 
-            e.printStackTrace(); 
-        } 
+            e.printStackTrace();
+        }
     }
 } 
   
